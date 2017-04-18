df8866638bd54e459e1bd47770d1d8d713cfaa53
Pamela Needle <pneedle@Pams-MacBook-Pro.local>
2017-04-17 21:30:32 -0400

    started a bash script to do dig on subdomains

---
+++ parser.py
@@ -0,0 +1,47 @@
+import re
+import collections
+import sys
+import csv
+import json
+import pytricia
+import ipaddress
+
+
+
+pyt = pytricia.PyTricia()
+def extract_subdomains():
+    with open('ALL_subdomains_Alexa_top1m.csv','rt') as alexa_subdomains, sys.stdout as subdomain_file:
+        reader = csv.DictReader(alexa_subdomains, delimiter='#')
+        for line in reader:
+            try:
+                subdomain_file.write(line['subdomain'])
+            except (BrokenPipeError, IOError):
+                pass
+        sys.stderr.close()
+
+            #print(line['subdomain'])
+
+def ipfunctin():
+    with open('ALL_subdomains_Alexa_top1m.csv','rt') as alexa_subdomains, open('ip-ranges.json','rt') as public_ips, open('subdomains.txt','w') as subdomains:
+        reader = csv.DictReader(alexa_subdomains,delimiter='#')
+
+        jsondata = json.load(public_ips)
+
+        for d in jsondata["prefixes"]:
+            ip = d['ip_prefix']
+            pyt.insert(ip,str(d['region']))
+
+        count = 0
+        for line in reader:
+            if count<50:
+                subdomains.write(line['subdomain']+"\n")
+            count +=1
+            #line = line.split("#")
+
+            #ip = ipaddress.ip_address(line["ip_address"]) #convert the IP address to IPv4
+            #print ip_address
+            #if ip in pyt:
+            # count+=1
+            #print line['alexa_rank'], line['subdomain'],ip_address, "found in ", pyt[ip_address]
+        print(count)
+
---
+++ parser.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ------------------------------------
+# SETUP
+# Please do not modify this section.
+
+# bash command-line arguments are accessible as $0 (the bash script), $1, etc.
+echo "Running" $0 "on" $1
+
+input_file=$1
+ulimit -v 1000000   # limit the virtual memory to 1000000 bytes = 1 MB
+# ------------------------------------
+
+# ------------------------------------
+# STUDENT CODE
+# Please replace this section with your solution.
+
+# Example: This will parse the first 20 lines of $input_file and write the results
+# to both STDOUT and to a file called example.csv.
+
+echo -e "Here is an example of streaming data into a python function:\n\n"
+
+# you can use \ break up a long sequence of unix pipes onto separate lines...
+# just make sure that "\" is the last character on each line
+#head -n 100
+python3 -c 'import parser; parser.extract_subdomains()' | tee temp.txt | dig -f temp.txt +noall +answer > new.txt
+
+
