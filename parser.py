b4af8b2ca8339e77d01a1c7ca03b68008804f32b
Pamela Needle <pneedle@Pams-MacBook-Pro.local>
2017-04-23 12:39:28 -0400

    wip transitioning to vagrant

--- parser.py
+++ parser.py
@@ -9,8 +9,10 @@ import ipaddress
 
 
 pyt = pytricia.PyTricia()
+
 def extract_subdomains():
     with open('ALL_subdomains_Alexa_top1m.csv','rt') as alexa_subdomains, sys.stdout as subdomain_file:
+        #open('temp.txt','w')
         reader = csv.DictReader(alexa_subdomains, delimiter='#')
         for line in reader:
             try:
@@ -21,27 +23,38 @@ def extract_subdomains():
 
             #print(line['subdomain'])
 
-def ipfunctin():
-    with open('ALL_subdomains_Alexa_top1m.csv','rt') as alexa_subdomains, open('ip-ranges.json','rt') as public_ips, open('subdomains.txt','w') as subdomains:
-        reader = csv.DictReader(alexa_subdomains,delimiter='#')
-
+def build_pyt():
+    '''build pyt dictionary from the published ips from amazon... so far 1099999
+    53155465 more subdomains to test. have completed 1323641'''
+    with open('ip-ranges.json','rt') as public_ips:
         jsondata = json.load(public_ips)
 
-        for d in jsondata["prefixes"]:
-            ip = d['ip_prefix']
-            pyt.insert(ip,str(d['region']))
+        for item in jsondata['prefixes']:
+            pyt.insert(item['ip_prefix'], item['region'])  # insert into pyt
 
-        count = 0
+def crossref_subdomainip(dns_output_file):
+    build_pyt()
+    with open(dns_output_file,'r') as dnslookups, open('subdomains.txt','w') as crossref_subdomains:
+        reader = csv.DictReader(dnslookups,delimiter=' ')
+        writer = csv.DictWriter(crossref_subdomains,['rank','subdomain','subdomainip'])
+        writer.writeheader()
+
+        count=0 #count the number of ips that are found wihtin an amazon public ip range
         for line in reader:
-            if count<50:
-                subdomains.write(line['subdomain']+"\n")
-            count +=1
-            #line = line.split("#")
+            if line[3]== 'CNAME': #ignore queries that were redirected to cname
+                continue
+            subdomain = line[0]
+            ip = line[4]
+            if ip in pyt: #if this ip is within an amazon ip range
+                count+=1
+                #what does pyt[ip] show
+                writer.writerow({'rank':'todo','subdomain':subdomain,'subdomainip':ip})
 
-            #ip = ipaddress.ip_address(line["ip_address"]) #convert the IP address to IPv4
-            #print ip_address
-            #if ip in pyt:
-            # count+=1
-            #print line['alexa_rank'], line['subdomain'],ip_address, "found in ", pyt[ip_address]
-        print(count)
+
+
+
+
+
+
+
 
