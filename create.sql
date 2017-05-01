0437d747afb36f5ef5db025b56d8f08b9202a9a6
Francisco Flores <fflores@colgate.edu>
2017-05-01 21:19:55 +0000

    create db

--- create.sql
+++ create.sql
@@ -1,6 +1,7 @@
 
 DROP TABLE IF EXISTS top1msubdomains CASCADE;
 DROP TABLE IF EXISTS top1mdomains CASCADE;
+DROP TABLE IF EXISTS dnsresults CASCADE;
 
 CREATE TABLE top1msubdomains (
     alexa_rank integer NOT NULL,
@@ -12,11 +13,19 @@ CREATE TABLE top1mdomains (
     domain text NOT NULL
 );
 
+CREATE TABLE dnsresults (
+    subdomain text NOT NULL,
+    ip text NOT NULL
+);
+
 COPY top1msubdomains FROM '/vagrant/projectfrankiepam/uniquewithrank.csv' WITH
 (FORMAT csv, HEADER true, DELIMITER ' ');
 
 COPY top1mdomains FROM '/vagrant/projectfrankiepam/top-1mdomains.csv' WITH
 (FORMAT csv, HEADER true, DELIMITER ',');
+
+COPY dnsresults FROM '/vagrant/projectfrankiepam/dnsresults1.txt' WITH
+(FORMAT text, DELIMITER ' ');
 
 ALTER TABLE ONLY top1msubdomains
     ADD CONSTRAINT top1msub_pkey PRIMARY KEY (alexa_rank,subdomain);
@@ -26,3 +35,9 @@ ALTER TABLE ONLY top1msubdomains
 
 ALTER TABLE ONLY top1mdomains
     ADD CONSTRAINT top1mdom_pkey PRIMARY KEY (rank);
+
+ALTER TABLE ONLY dnsresults
+    ADD CONSTRAINT dnsresults_pkey PRIMARY KEY (subdomain, ip);
+
+ALTER TABLE ONLY dnsresults
+    ADD CONSTRAINT dnsresults_fkey FOREIGN KEY (subdomain) REFERENCES top1msubdomains(subdomain);
